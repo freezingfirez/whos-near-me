4cc4ae923063995c714a3ed97c2d3c112f6bc232
freezingfirez <nathan@fischdesigns.com>
2025-08-08 19:04:00 -0400

    fix: Provide profilePictureUrl in Profile default init

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    username: {
+    email: {
         type: String,
         required: true,
         unique: true,
+        trim: true,
+        lowercase: true
+    },
+    username: {
+        type: String,
+        required: true,
         trim: true
     },
     password: {
